9d1caf986c8eb1b7eecf48d5df9a136eda8cb4f6
Abdirahim Ali <abdichange@gmail.com>
2024-09-05 15:23:11 +0300

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,6 @@ module.exports = {
         'mobile': "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/assets/img/mobile/image-footer.jpg')",
         'tablet': "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/assets/img/tablet/image-footer.jpg')",
         'desktop': "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/assets/img/desktop/image-footer.jpg')",
-        'custom-dual' : "url('/assets/img/desktop/image-hero-left.png'), url('/assets/img/desktop/image-hero-right.png')",
       }
     },
   },
